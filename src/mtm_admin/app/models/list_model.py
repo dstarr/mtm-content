2d9a7d7fbaa4de5b1075bfe2b79d64ca76a29c1d
David Starr <david@elegantcode.com>
2024-01-16 14:06:26 -0800

    safety commit

---
+++ src/mtm_admin/app/models/list_model.py
@@ -0,0 +1,10 @@
+class ListModel():
+    
+    def __init__(self, items):
+        self.items = items
+
+class ListItemModel():
+    
+    def __init__(self, playlist, module):
+        self.playlist = playlist
+        self.module = module
