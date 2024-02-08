eda54720967f8faa7ea34b4f898ee2e9fe2d72ed
David Starr <dastarr@microsoft.com>
2024-02-08 05:57:19 -0800

    removed support for transcript upload

--- src/mtm_admin/services/file_service.py
+++ src/mtm_admin/services/file_service.py
@@ -15,12 +15,6 @@ class FileType(Enum):
         "display_name": "Slides",
         "content_type": "slides"
     }
-    TRANSCRIPT = {
-        "container_name": config.BLOB_STORAGE_CONTAINER_NAME_TRANSCRIPTS,
-        "content_key": "transcript_url",
-        "display_name": "Transcript",
-        "content_type": "transcript"
-    }
     VIDEO = {
         "container_name": config.BLOB_STORAGE_CONTAINER_NAME_VIDEO,
         "content_key": "video_url",
@@ -33,53 +27,39 @@ class FileType(Enum):
         "display_name": "Sample code",
         "content_type": "sample_code"
     }
-    OTHER = {
-        "container_name": config.BLOB_STORAGE_CONTAINER_NAME_OTHER,
-        "content_key": "other_url",
-        "display_name": "Other",
-        "content_type": "other"
-    }
-
+    
 class FileService():
     def upload_to_blob(self, blob_name, content, file_type: FileType):
-        try:
-            blob_service_client = BlobServiceClient.from_connection_string(config.BLOB_STORAGE_CONNECTION_STRING)
+        
+        container_name = file_type.value["container_name"]
+        
+        blob_service_client = BlobServiceClient.from_connection_string(config.BLOB_STORAGE_CONNECTION_STRING)
+        container_client = blob_service_client.get_container_client(container_name)
+        
+        # ensure the container exists
+        if not container_client.exists():
+            container_client.create_container()
+        
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        blob_client.upload_blob(content, overwrite=True)
 
-            blob_client = blob_service_client.get_blob_client(container=file_type.value["container_name"], blob=blob_name)
-
-            blob_client.upload_blob(content, overwrite=True)
-
-            return blob_client.url
-
-        except Exception as ex:
-            print('Exception:')
-            print(ex)
+        return blob_client.url
 
     def get_blob_from_storage(self, blob_name, file_type: FileType):
-        try:
-            blob_client = BlobServiceClient.from_connection_string(config.BLOB_STORAGE_CONNECTION_STRING).get_blob_client(container=file_type, blob=blob_name)
+        blob_client = BlobServiceClient.from_connection_string(config.BLOB_STORAGE_CONNECTION_STRING).get_blob_client(container=file_type, blob=blob_name)
 
-            print(f"Downloading blob: {blob_name} from Azure Storage")
-            blob_data = blob_client.download_blob().readall()
-            print("Download successful")
+        print(f"Downloading blob: {blob_name} from Azure Storage")
+        blob_data = blob_client.download_blob().readall()
+        print("Download successful")
 
-            return blob_data
-
-        except Exception as ex:
-            print('Exception:')
-            print(ex)
+        return blob_data
 
     def delete_blob_in_storage(self, blob_name, file_type: FileType):
         container_name = file_type.value["container_name"]
-        try:
-            blob_service_client = BlobServiceClient.from_connection_string(config.BLOB_STORAGE_CONNECTION_STRING)
-            blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        blob_service_client = BlobServiceClient.from_connection_string(config.BLOB_STORAGE_CONNECTION_STRING)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
-            if blob_client.exists():
-                print(f"Deleting blob: {blob_name} from Azure Storage")
-                blob_client.delete_blob()
-                print("Deletion successful")
-
-        except Exception as ex:
-            print('Exception:')
-            print(ex)
+        if blob_client.exists():
+            print(f"Deleting blob: {blob_name} from Azure Storage")
+            blob_client.delete_blob()
+            print("Deletion successful")
